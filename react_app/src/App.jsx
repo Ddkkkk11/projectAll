ed36891b8b8715957836af4e677565cdd90d43c4
xdd <45672942@qq.com>
2022-09-24 14:19:32 +0800

    delete data

--- react_app/src/App.jsx
+++ react_app/src/App.jsx
@@ -1,52 +1,55 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 import StudentList from './components/StudentList'
 import './App.css'
+import StuContent from "./stuContent";
 
 const App = () => {
 
-  const [stuData, setStuData] = useState([])
+    const [stuData, setStuData] = useState([])
 
-  // 添加一个state来记录数据是否正在加载,false表示没有加载数据，true表示加载
-  const [loading, setLoading] = useState(false)
+    // 添加一个state来记录数据是否正在加载,false表示没有加载数据，true表示加载
+    const [loading, setLoading] = useState(false)
 
-  // 创建一个state来记录错误信息
-  const [error, setError] = useState(null)
+    // 创建一个state来记录错误信息
+    const [error, setError] = useState(null);
 
-  //抽出请求方法
-  const fetchData = useCallback(async () => {
-      try {
-        setLoading(true)
-        setError(null)
-        const res = await fetch('http://localhost:1337/api/students')
-        //判断请求是否加载成功
-        if (res.ok) {
-          const data = await res.json()
-          console.log(data.data)
-          setStuData(data.data)
-        } else {
-          throw new Error('数据加载失败！')
+    //抽出请求方法
+    const fetchData = useCallback(async () => {
+        try {
+            setLoading(true)
+            setError(null)
+            const res = await fetch('http://localhost:1337/api/students')
+            //判断请求是否加载成功
+            if (res.ok) {
+                const data = await res.json();
+                console.log(data.data)
+                setStuData(data.data)
+            } else {
+                throw new Error('数据加载失败！')
+            }
+        } catch (e) {
+            setError(e)
+        } finally {
+            setLoading(false)
         }
-      } catch (e) {
-        setError(e)
-      } finally {
-        setLoading(false)
-      }
 
     })
 
 
-  useEffect(() => {
-    fetchData()
-  }, [])
+    useEffect(() => {
+        fetchData();
+    }, [])
 
-  return (
-    <div className="app">
-      <button onClick={() => fetchData()}>点我加载数据</button>
-      {(!loading && !error) && <StudentList stus={stuData}/>}
-      {loading && <p>数据正在加载中...</p>}
-      {error && <p>数据加载异常！</p>}
-    </div>
-  )
+    return (
+        <StuContent.Provider value={{fetchData}}>
+            <div className="app">
+                <button onClick={() => fetchData()}>点我加载数据</button>
+                {(!loading && !error) && <StudentList stus={stuData}/>}
+                {loading && <p>数据正在加载中...</p>}
+                {error && <p>数据加载异常！</p>}
+            </div>
+        </StuContent.Provider>
+    )
 }
 
 export default App
--- react_app/src/components/Student.jsx
+++ react_app/src/components/Student.jsx
@@ -1,26 +1,58 @@
-import React from 'react'
+import React, {useCallback, useContext, useState} from 'react'
+import stuContext from "../stuContent";
 
 const Student = (props) => {
-  // {stu:{name, age, gender, address}} = props
-  console.log(props.stu.id)
-  const deleteHandle = (props) => {
-    //删除接口 http://localhost:1337/api/students/4
-    console.log(props.stu.id)
-    fetch(`http://localhost:1337/api/students/${props.stu.id}`);
+    // {stu:{name, age, gender, address}} = props
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    console.log(props);
+    const ctx = useContext(stuContext);
+    const delStu = useCallback(async () => {
+        try {
+            setLoading(true);
+            setError(false);
+            const res = await fetch(`http://localhost:1337/api/students/${props.stu.id}`, {
+                method: 'delete'
+            });
 
-  };
+            //判断是否成功
+            if (!res.ok) {
+                throw new Error('删除数据失败');
+            }
 
-  return (
-    <tr>
-      <td>{props.stu.attributes.name}</td>
-      <td>{props.stu.attributes.gender}</td>
-      <td>{props.stu.attributes.age}</td>
-      <td>{props.stu.attributes.address}</td>
-      <td>
-        <button onClick={deleteHandle}>删除</button>
-      </td>
-    </tr>
-  )
+            //修改数据后需要触发列表重新刷新
+            ctx.fetchData();
+            // const data = await res.json();//被删除的学生
+        } catch (e) {
+            setError(e);
+        } finally {
+
+        }
+
+
+    }, [])
+    const deleteHandle = (props) => {
+        //删除接口 http://localhost:1337/api/students/4
+        // console.log(props.stu.id)
+        delStu()
+
+    };
+
+    return (
+        <>
+            <tr>
+                <td>{props.stu.attributes.name}</td>
+                <td>{props.stu.attributes.gender}</td>
+                <td>{props.stu.attributes.age}</td>
+                <td>{props.stu.attributes.address}</td>
+                <td>
+                    <button onClick={deleteHandle}>删除</button>
+                </td>
+            </tr>
+            {loading && <tr colSpan={5}>正在删除数据</tr>}
+            {error && <tr colSpan={5}>删除失败</tr>}
+        </>
+    )
 }
 
 export default Student
